763c40bf82324f6433b0c6a7d43fcab7acbad758
maxilagv <maxilavagetto@gmail.com>
2025-07-23 15:23:44 -0300

    Add .next/static/chunks/src_app_credit-scoring_page_tsx_a993527f._.js 1753295024131

---
+++ .next/static/chunks/src_app_credit-scoring_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_abd1aa74._.js",
+  "static/chunks/node_modules_0ebfa638._.js"
+],
+    source: "dynamic"
+});
